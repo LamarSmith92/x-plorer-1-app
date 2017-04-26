02bfcbf571439f47a1a76a8550f797ed62aac96f
Chris Duran <chris.duran17@yahoo.com>
2017-04-26 16:06:05 -0700

    rendered seeded data onto show page. Continuing to show more data + beautify

--- xplorer1/app/controllers/satellites_controller.rb
+++ xplorer1/app/controllers/satellites_controller.rb
@@ -1,2 +1,29 @@
 class SatellitesController < ApplicationController
+
+  def  index
+    @satellites = Satellite.all
+  end
+
+  def show
+    sat_id = params[:id]
+    @satellite = Satellite.find_by_id(sat_id)
+  end
+
+  def new
+    @satellite = Satellite.new
+  end
+
+    def create
+      @satellite = Satellite.new(satellite_params)
+    end
+
+    def destroy
+      @satellite = Satellite.find(params[:id])
+      @satellite.destroy
+    end
+
+    private
+    def post_params
+      params.require(:satellite).permit(:name, :picture, :description)
+    end
 end
