b4f5bddea4be55b988b6de5ec83660010b0f99fb
shaya nelson <shayanelson@gmail.com>
2017-04-27 11:43:34 -0700

    updating master

--- xplorer1/db/seeds.rb
+++ xplorer1/db/seeds.rb
@@ -8,6 +8,12 @@
 
 Satellite.destroy_all
 
-   Satellite.create(:id => 1, :name=>'I.S.S', :picture=>'', :description=>'Floating space station in the orbit of the Earth that you can see if you have a fucking telescope, but not with your camera.')
+Satellite.create(:id => 1, :name=>'I.S.S', :picture=>'', :description=>'Floating space station in the orbit of the Earth that you can see if you have a telescope, but not with your camera.')
 
-   Satellite.create(:id => 2, :name=>'Some other satellite', :picture=>'', :description=>'this is another satellite that\'s floating above the fucking earth. Save our fucking planet, SHAYA!!!.')
+Satellite.create(:id => 2, :name=>'Some other satellite', :picture=>'', :description=>'this is another satellite that\'s floating above the earth. Save our planet.')
+
+Satellite.create(:id => 3, :name=>'Satellite 3', :picture=>'', :description=>'The history of satellite 3')
+
+Satellite.create(:id => 4, :name=>'Satellite 4', :picture=>'' :description=>'The history of satellite 4')
+
+Satellite.create(:id => 5, :name=>'Satellite 5', :pictures=>'' :description=>'The history of satellite 5')
