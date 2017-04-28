e0a7a7a9bb1a94c585f5130401aebff5b10f236e
Lamar Smith <Lamar5503@me.com>
2017-04-28 16:16:25 -0700

    bug fixture

--- xplorer1/app/controllers/comments_controller.rb
+++ xplorer1/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   before_action :find_satellite
-  before_action :find_comment, only: [:destroy]
-
+  before_action :find_comment, only: [:destroy, :comment_owner]
+  before_action :comment_owner, only: [:destroy]
   def create
     @satellite = Satellite.find(params[:satellite_id])
     @comment = @satellite.comments.create(params[:comment].permit(:title, :content))
@@ -9,7 +9,6 @@ class CommentsController < ApplicationController
     @comment.save
 
     if @comment.save
-
     redirect_to satellite_path(@satellite)
   else
     render 'new'
@@ -29,6 +28,10 @@ end
 
   def find_comment
     @comment = @satellite.comments.find(params[:id])
-  
+end
+
+  def comment_owner
+    unless current_user.id == @comment.user_id
+end
 end
 end
