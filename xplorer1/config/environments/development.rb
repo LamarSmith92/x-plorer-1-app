9e3ce295bd9929c7ef85e3773ce04feca57d788e
shaya nelson <shayanelson@gmail.com>
2017-04-28 15:30:59 -0700

    updated ISS info, removed log-out prompt

--- xplorer1/config/environments/development.rb
+++ xplorer1/config/environments/development.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
@@ -51,5 +50,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-    config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
