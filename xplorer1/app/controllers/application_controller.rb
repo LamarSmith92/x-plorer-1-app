275d95744a1e76373e09476188601ed2e41bfe88
Lamar Smith <Lamar5503@me.com>
2017-05-01 13:41:26 -0700

    .

--- xplorer1/app/controllers/application_controller.rb
+++ xplorer1/app/controllers/application_controller.rb
@@ -8,8 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name,  :birthday, :email, :password, :birthday, :city, :state, :birthday, :remember_me)}
     devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :password, :remember_me)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :birthday, :email, :password, :about, :current_password, :remember_me)}
+    redirect_to user_path(@user.id)
+
   end
-  
+
   private
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
