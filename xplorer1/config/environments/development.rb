e75257eec0302a6e52735a9329d1b6ff8210fd70
Ali Emami <emamiali1993@gmail.com>
2017-05-01 15:29:44 -0700

    changed the google api key location

--- xplorer1/config/environments/development.rb
+++ xplorer1/config/environments/development.rb
@@ -50,5 +50,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 } || { host : 'https://stark-earth-33379.herokuapp.com/'}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
