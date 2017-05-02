4d1a943f28841ac560e6092c57926d803807f08b
Lamar Smith <Lamar5503@me.com>
2017-05-01 21:15:25 -0700

    styling tweaks

--- xplorer1/app/models/user.rb
+++ xplorer1/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+         has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOOMOpjdy88srHLrEfMmZB_HohTpLa_dyb4OLy0IQ7Ej6QEBQrE_ElDxA"
          validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
          has_many :comments
